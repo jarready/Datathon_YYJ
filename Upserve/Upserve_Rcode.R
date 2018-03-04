35def3c0eba39250f78b9f2b48da6570990a98fa
Annie Yang <xiaofei_yang@brown.edu>
2018-03-04 13:18:32 -0500

    Add comments

--- Upserve/Upserve_Rcode.R
+++ Upserve/Upserve_Rcode.R
@@ -1,3 +1,11 @@
+# install packages
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(MASS)
+library(reshape)
+library(readr)
+
 # Import dataset
 setwd("/Users/yangxiaofei/Desktop/datathon/Upserve")
 
@@ -18,8 +26,8 @@ overall <- overall %>% mutate(Month=as.numeric(format(overall$date,"%m")), # Cre
 overall %>%
   mutate(Month2 = as.Date(paste0("2017-", Month,"-01"),"%Y-%m-%d"))%>%
   dplyr::group_by(Month2, shift_bin, region)%>%
-  dplyr::summarise(avg_total_sales = mean(total_sales))%>%
-  ggplot(aes(x = Month2, y = avg_total_sales, colour = shift_bin)) +
+  dplyr::summarise(avg_total_sales = mean(total_sales))%>% # Calculate monthly sales
+  ggplot(aes(x = Month2, y = avg_total_sales, colour = shift_bin)) + # Show sales trend of different shift_bin
   geom_line()+
   facet_wrap(~region,scales="free_y")+
   theme_bw()+
@@ -87,7 +95,7 @@ ggplot(c_adjust, aes(item_category, region_time)) +
 
 ggsave("heatmap_AvgPrice_adjust.png")
 
-# heatmap_AvgItem
+# Plot heatmap showing average items
 avg_items_hm<-catehm%>%
   filter(total_sales!=0)%>% # delete free item
   dplyr::group_by(region_time, item_category)%>%
@@ -115,7 +123,7 @@ cate_trend <- clean_data2 %>%
   dplyr::group_by(Month,region,item_category)%>%
   dplyr::summarise(avg_sale = mean(total_sales))
 
-
+# Monthly Average Sales Trend by Category
 clean_data2%>%
   mutate(Month2 = as.Date(paste0("2017-", Month,"-01"),"%Y-%m-%d")) %>%
   dplyr::group_by(Month2,region,item_category)%>%
@@ -132,6 +140,7 @@ clean_data2%>%
 
 ggsave("Monthly_Average_Sales_Category.png")
 
+# Monthly Average Items Trend by Category
 clean_data2%>%
   mutate(Month2 = as.Date(paste0("2017-", Month,"-01"),"%Y-%m-%d")) %>%
   dplyr::group_by(Month2,region,item_category)%>%
@@ -192,27 +201,35 @@ west%>%
 ggsave("WestFree.png")
 
 # Restaurant Clustering
-
+# Create Price_Level factor vector
 Price_Level <- c("$9.24", "$$21.70","$$$52.17","$$$$103.24")
 
+# Create a vector holding the number of restaurant in 
+# Northeast - the number is calculated using the label from k-means clustering
 Northeast <- c(67,22,11,0)
 
+# Create a vector holding the number of restaurant in Midwest
 Midwest <- c(81,17,0,2)
 
+# Create a vector holding the number of restaurant in West
 West<-c(85,13,2,0)
 
+# Create a vector holding the number of restaurant in South
 South<-c(74,20,2,4)
 
+# Combine vectors and create a dataframe
 R_level <- as.data.frame(cbind(Price_Level,Northeast, Midwest, West, South))
 
-library(reshape)
-
+# Reshape dataframe
 R_level1<-melt(R_level, id.vars = 1, value.name="Restaurant_Num", variable_name = "Region")
 
+# Change factor to numeric
 R_level1$value<-as.numeric(levels(R_level1$value))[R_level1$value]
 
+# Change column name
 colnames(R_level1)[3] <- "Number_of_Restaurant"
 
+# Plot heatmap showing number of restaurants of different price levels in different regions
 ggplot(R_level1, aes(Price_Level, Region)) + 
   theme_bw()+
   geom_tile(aes(fill = Number_of_Restaurant),colour = "white") + 
